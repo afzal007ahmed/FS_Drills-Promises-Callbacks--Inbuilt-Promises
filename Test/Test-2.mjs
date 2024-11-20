872535efe505c01cd561e71cd9f0d4a50c32c733
afzal007ahmed <afzalahmeddc@gmail.com>
2024-11-20 10:34:01 +0530

    changes

--- Test/Test-2.mjs
+++ Test/Test-2.mjs
@@ -2,13 +2,4 @@ import {readFiles , toDelete , toLower , toSort , toUpper } from "../Problem-2.j
 
 
 // 1 -2 . Read file and make a new file with uppercase .
-readFiles( '/home/afzal-ahmed/Downloads/JS-FS-Drills/Test/' , 'lipsum.txt' , toUpper , 'lipsum-upper.txt' ) ;
-
-// 3. make the content lowercase 
-readFiles( '/home/afzal-ahmed/Downloads/JS-FS-Drills/Test/' , 'lipsum-upper.txt' , toLower  , 'lipsum-lower.txt' ) ;
-
-// 4. make the content sorted  
-readFiles( '/home/afzal-ahmed/Downloads/JS-FS-Drills/Test/' , 'lipsum-upper.txt' , toSort , 'sorted-lipsum.txt' ) ;
-
-// 5. Delete all the files. 
-readFiles( '/home/afzal-ahmed/Downloads/JS-FS-Drills/Test/' , 'filenames.txt' , toDelete ) ;
+readFiles( '/home/afzal-ahmed/Downloads/JS-FS-Drills/Test/' , 'lipsum.txt' , toUpper , toLower , toSort , toDelete ) ;
